ea8db2aee33930ffcea0d596006fdb1ad95a86ea
Albaro Hernandez <hernandezalbaro.23@gmail.com>
2022-04-08 15:11:28 -0300

    second commit

--- project_app/urls.py
+++ project_app/urls.py
@@ -2,10 +2,11 @@
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, path
+from django.urls import path
 from .views import list_family
 
 urlpatterns = [
     
-    path('', list_family)
+    path('', list_family),
+    
 ]
--- project_app/views.py
+++ project_app/views.py
@@ -1,17 +1,28 @@
-from re import template
+from unittest import loader
 from django.shortcuts import render
-from django.template import loader
+from django.template.loader import get_template
 from .models import Familia
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 
 def list_family(request):
-    template= loader.get_template('list_family.html')
+    #template= get_template('list_family.html')
     familia= Familia.objects.all()
     #print(familia)
     context= {
-        'familia': familia        
+        'familia': familia          
     }
-    return HttpResponse(template.render(context, request))
+    return render(request,"list_family.html", context)
+
+def framework(request):
+    fecha_actual= datetime.now()
+    #temp= get_template('plantilla_hija.html')
+    ctx= {
+        "dame_fecha":fecha_actual
+        }
+
+    return render(request,'plantilla_hija.html',ctx)
+    
      
